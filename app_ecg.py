35645694ef86763a61b184db105b8ba569644b4f
Alew <xalejandrow@gmail.com>
2022-08-27 01:01:30 +0000

    add final predictions and titles

--- app_ecg.py
+++ app_ecg.py
@@ -14,15 +14,30 @@ def calcPrediction(data):
     model = pickle.load(open('model.pkl','rb'))
     df = data.iloc[:,:-1]
     result = model.predict(df)
-    st.text(result)
+    #st.text(result)
     test_result = result.tolist()[0]
-    st.text(test_result)
+    #st.text(test_result)
+    return test_result
 
+pred = None
 if file is not None:
     df= pd.read_csv(file, header=None)
     st.dataframe(df)
     st.title('ECG Graph')
     st.line_chart(df.iloc[0,:186])
     #calcPrediction(df[0,0:186])
-    calcPrediction(df)
+    pred = calcPrediction(df)
     #calcPrediction(df[:,-1])
+
+
+tabla_switch = {
+    0.0:'Normal',
+    1.0:'Supraventricular',
+    2.0:'Ventricular',
+    3.0:'Fusion',
+    4.0:'Unknown'
+}
+
+if pred is not None:
+    st.header('Resultado del ECG:')
+    st.subheader(tabla_switch[pred])
