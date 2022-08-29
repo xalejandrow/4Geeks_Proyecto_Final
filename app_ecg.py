bf062d4f3ff74f1a8e6ddd80b375b3ed2251ef6f
Alew <xalejandrow@gmail.com>
2022-08-29 15:08:36 +0000

    comment upload model

--- app_ecg.py
+++ app_ecg.py
@@ -8,14 +8,14 @@ import pickle
 #adding a file uploader
 
 # Upload Model
-file_model = st.file_uploader("Please choose a Model file if not uploaded yet")
+#file_model = st.file_uploader("Please choose a Model file if not uploaded yet")
 # check if the file has been uploaded
-if file_model:
+#if file_model:
     # strip the leading path from the file name
-    fn = os.path.basename(file_model)
+    #fn = os.path.basename(file_model)
      
    # open read and write the file into the server
-    open(fn, 'wb').write(fileitem.file.read())
+   # open(fn, 'wb').write(fileitem.file.read())
 
 # Upload CSV
 file = st.file_uploader("Please choose a file")
