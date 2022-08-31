e02b6c6e0d5d366b8b19faf618c5e5e3fee93387
Alew <xalejandrow@gmail.com>
2022-08-31 22:44:35 +0000

    tabla mejorada y agreagddo descripcion de graficas

--- app_ecg.py
+++ app_ecg.py
@@ -15,11 +15,12 @@ st.caption('fuente: https://fisiologia.facmed.unam.mx/index.php/taller-de-interp
 #adding a file uploader
 
 st.header('Clases detectadas')
-st.image('img/Graficas_de_Clases.png')
+st.image('img/Graficas_de_Clases.png', caption='Elaboración propia')
 
 #imprimo tabla de clases
 #df_clases['Clase'] = {0,1,2,3,4}
-clases = {'Clase':[0,1,2,3,4],'Name':['Normal','Supraventricular','Ventricular','Fusion','Unknown']}
+#clases = {'Clase':[0,1,2,3,4],'Name':['Normal','Supraventricular','Ventricular','Fusion','Unknown']}
+clases = {'Name':['Normal','Supraventricular','Ventricular','Fusion','Unknown']}
 df_clases = pd.DataFrame(clases)
 df_clases.reset_index()
 #df_clases = []
